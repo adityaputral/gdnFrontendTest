b85b8c653f057a0a241623fe7c9ae37f8d3505b2
Aditya Putra <adityaputra7997@gmail.com>
2019-07-13 13:14:50 +0700

    [ADD] Top Navigation
    - Banner help
    - Navigation bar
    
    Not yet implemented
    - Caret "Kategori"
    - Search icon inside search box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
